c262a56e9797f2f3875145ab9d9ee37c36db638d
Arlyson Mathias <mathiassouza10@outlook.com>
2022-05-30 11:24:22 -0300

    Projeto2

---
+++ assets/script.js
@@ -0,0 +1,240 @@
+//Classes
+class Requisicoes {
+  async buscarTodasTarefas() {
+    const response = await fetch(`${baseUrl}/listar-todas`);
+    const tarefas = await response.json();
+
+    listaDeTarefas = tarefas;
+
+    return tarefas;
+  }
+  async buscarTarefaPorId(id) {
+    const response = await fetch(`${baseUrl}/tarefa/${id}`);
+    const tarefa = await response.json();
+    console.log(response);
+
+    return tarefa;
+  }
+
+  async criarTarefa(atividade) {
+    const tarefa = { atividade };
+
+    const response = fetch(`${baseUrl}/criar-tarefa`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+      body: JSON.stringify(tarefa),
+    });
+
+    const novaTarefa = await response.json();
+
+    return novaTarefa;
+  }
+
+  async atualizarTarefa(id, atividade) {
+    const tarefa = { atividade };
+
+    const response = await fetch(`${baseUrl}/atualizar-tarefa/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      mode: "cors",
+      body: JSON.stringify(tarefa),
+    });
+
+    const tarefaAtualizada = response.json();
+
+    return tarefaAtualizada;
+  }
+
+  async excluirTarefa(id) {
+    const response = await fetch(`${baseUrl}/deletar-tarefa/${id}`, {
+      method: "DELETE",
+      mode: "cors",
+    });
+
+    if (response.status == 204) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+//variável auxliar
+const baseUrl = "http://localhost:3000/tarefas";
+let listaDeTarefas = [];
+const requisicoes = new Requisicoes();
+
+// Funções de manipulação do HTML
+
+const imprimirTodasTarefas = async () => {
+  const tarefas = await requisicoes.buscarTodasTarefas();
+
+  document.getElementById("lista-tarefas").innerHTML = "";
+
+  tarefas.forEach((element) => {
+    document.getElementById("lista-tarefas").insertAdjacentHTML(
+      "beforeend",
+      `
+    <div class="lista-tarefas">  
+      <ul id="lista-tarefas-ul">
+            <li>               
+                <span class="textoTarefa">${element.atividade}</span>
+                <div>
+                <button onclick="abrirModalEdit('${element._id}')" class="btnEditar">
+                     <i class="fa-solid fa-pencil"></i>
+                </button>
+                <button onclick="abrirModalDelete('${element._id}')" class="btnApagar">
+                  <i class="fa-solid fa-trash-can"></i>
+                </button>
+                </div>
+            </li>
+      </ul>
+    </div>
+    `
+    );
+  });
+};
+
+// mostrar na tela por ID
+
+const imprimirTarefaPorId = async () => {
+  const input = document.getElementById("pesquisar-nome");
+  const atividade = input.value;
+
+  const tarefaSelecionada = listaDeTarefas.find(
+    (element) => element.atividade === atividade
+  );
+
+  const tarefa = await requisicoes.buscarTarefaPorId(tarefaSelecionada._id);
+
+  document.getElementById("atividade-escolhida");
+
+  document.getElementById("atividade-escolhida").innerHTML = `
+      
+        <ul id="lista-tarefas">
+              <li>                
+                  <span class="textoTarefa">${tarefa.atividade}</span>
+                  <div>
+                  <button onclick="abrirModalEdit('${tarefa._id}')" class="btnEditar">
+                      <i class="fa-solid fa-pencil"></i>
+                  </button>
+                  <button onclick="abrirModalDelete('${tarefa._id}')" class="btnApagar">
+                    <i class="fa-solid fa-trash-can"></i>
+                  </button>
+                  </div>
+              </li>
+        </ul>    
+      `;
+};
+
+//função cadastrar tarefa
+
+async function cadastrarNovaTarefa() {
+  const atividade = document.getElementById("novaTarefa").value;
+
+  const tarefa = requisicoes.criarTarefa(atividade); // await dando erro na parte visual
+
+  document.getElementById("lista-tarefas").insertAdjacentHTML(
+    "beforeend",
+    `
+      <div class="lista-tarefas">  
+        <ul id="lista-tarefas-ul">
+              <li>               
+                  <span class="textoTarefa">${tarefa.atividade}</span>
+                  <div>
+                  <button onclick="abrirModalEdit('${tarefa._id}')" class="btnEditar">
+                      <i class="fa-solid fa-pencil"></i>
+                  </button>
+                  <button onclick="abrirModalDelete('${tarefa._id}')"  class="btnApagar">
+                    <i class="fa-solid fa-trash-can"></i>
+                  </button>
+                  </div>
+              </li>
+        </ul>
+      </div>`
+  );
+  document.getElementById("novaTarefa").value = "";
+
+  imprimirTodasTarefas();
+}
+
+// MODAIS
+
+//Abrir e fechar modais de pesquisa
+function abrirModalPesquisa() {
+  const nome = document.getElementById("pesquisar-nome").value;
+
+  const tarefaSelecionada = listaDeTarefas.find(
+    (element) => element.atividade === nome
+  );
+
+  if (tarefaSelecionada) {
+    document.querySelector(".modal-overlay").style.display = "flex";
+    imprimirTarefaPorId(tarefaSelecionada);
+  } else {
+    alert("Tarefa não encontrada.");
+  }
+
+  document.getElementById("pesquisar-nome").value = "";
+}
+
+function fecharModal() {
+  document.querySelector(".modal-overlay").style.display = "none";
+}
+
+// modal deletar
+
+function abrirModalDelete(id) {
+  document.querySelector(".modal-overlay-delete").style.display = "flex";
+
+  const btnConfirmar = document.getElementById("btn-apagar-yes");
+
+  btnConfirmar.addEventListener("click", async () => {
+    const exclusao = await requisicoes.excluirTarefa(id);
+
+    if (exclusao) {
+      fecharModalDelete();
+      alert("Tarefa Excluida com sucesso");
+      imprimirTodasTarefas();
+    } else {
+      fecharModalDelete();
+      alert("Tarefa Excluida com sucesso");
+      imprimirTodasTarefas();
+    }
+  });
+}
+
+function fecharModalDelete() {
+  document.querySelector(".modal-overlay-delete").style.display = "none";
+}
+
+function abrirModalEdit(id) {
+  document.querySelector(".modal-overlay-edit").style.display = "flex";
+
+  const tarefa = listaDeTarefas.find((element) => element._id === id);
+
+  document.getElementById("atualizarTarefa").value = tarefa.atividade;
+
+  const bntAtualizar = document.getElementById("btn-atualizar-tarefa");
+
+  bntAtualizar.addEventListener("click", async () => {
+    const atividade = document.getElementById("atualizarTarefa").value;
+
+    await requisicoes.atualizarTarefa(id, atividade);
+
+    imprimirTodasTarefas();
+    fecharModalEdit();
+  });
+}
+
+function fecharModalEdit() {
+  document.querySelector(".modal-overlay-edit").style.display = "none";
+}
+
+//chamando a função que imprime as tarefas na tela
+imprimirTodasTarefas();
